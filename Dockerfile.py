eec938af4cac495238b6149b59959920bd39426a
Yue Yu <yue.yu.aka.fish@outlook.com>
2022-04-06 03:11:45 +0100

    update Dockerfile

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,6 +1,7 @@
 FROM python:3.8
 
 COPY py/ /app/
+RUN apt-get update && apt-get install -y python3-opencv
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
 CMD [ "python", "/app/iot.py" ]
